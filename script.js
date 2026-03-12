46546d3e61ef4ff6c08756f76f5a7c5f431ab59c
ahmad-bhai <nasiriq768@gmail.com>
2026-03-12 15:04:50 +0500

    Add files via upload

---
+++ script.js
@@ -0,0 +1,7 @@
+console.log("SCRIPT VERSION 1");
+
+// example UI element
+document.body.insertAdjacentHTML(
+    "beforeend",
+    "<div style='position:fixed;bottom:20px;right:20px;background:black;color:white;padding:10px'>SERVER SCRIPT V1</div>"
+);
